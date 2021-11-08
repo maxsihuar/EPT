9de687ee2cca6c24978bb391ee926e63ae6860d8
Max Sihuar Holguino Nuñez <maxsihuar98@.gmail.com>
2021-11-08 18:09:07 -0500

    Se agrego un nuevo archivo html

--- JS/Main.js
+++ JS/Main.js
@@ -2,6 +2,12 @@ const Search =  document.getElementById("search");
 
 const Bar = document.getElementById("btn");
 
+let Submit = document.querySelector("#searcher");
+
+document.querySelector("form").addEventListener("submit",()=>{
+	event.preventDefault()
+})
+
 Bar.addEventListener('click', ()=>{
     document.querySelectorAll('li').forEach((element) =>{
       element.classList.toggle('responsive')
@@ -10,4 +16,12 @@ Bar.addEventListener('click', ()=>{
 
 function Seach(){
   Search.classList.toggle("display");
+}
+
+function onKeyDow(event){
+  let key = event.keyCode;
+  
+  if(key==13){
+    window.location.href = "https://www.google.com/search?q=" + Submit.value;
+  };
 }
