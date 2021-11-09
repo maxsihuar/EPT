e0195099b3e168e095f6ace2d2eb128aa94deec1
Max Sihuar Holguino Nuñez <maxsihuar98@.gmail.com>
2021-11-08 21:06:33 -0500

    Se agrego la funcion de compra

---
+++ JS/Card.js
@@ -0,0 +1,19 @@
+var Span;
+var Imag;
+
+function Main(){
+    document.querySelectorAll(".card").forEach((ele)=>{
+        ele.addEventListener("click", ()=>{
+            Span = ele.querySelector("span").innerHTML;
+            Imag = ele.querySelector("img").src;
+            localStorage.setItem("Proper", Span);
+            localStorage.setItem("Imagen", Imag);
+            window.location.href = "Compra.html";
+        })
+    })
+}
+function Property(){
+
+    document.querySelector("title").innerText = localStorage.getItem("Proper");
+    document.querySelector("img").src = localStorage.getItem("Imagen");
+}
