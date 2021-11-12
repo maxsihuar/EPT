d83af644bf9948558908976642efdb18d3d34873
Max Sihuar Holguino Nuñez <maxsihuar98@.gmail.com>
2021-11-11 19:14:38 -0500

    Se añadio mas estilo y se remplazaron imagenes

--- JS/Description.js
+++ JS/Description.js
@@ -1,16 +1,43 @@
 var Count = 0;
+
+const Harina_Habas = "</br>"+"<span>Harina de habas peladas</span>"+"</br>"+"<span>Granos finamente molios</span>";
+const Kañiwa_tostada = "</br>"+"<span>Granos de Kañiwa tostadas</span>"+"</br>"+"<span>Granos tostados parejamente</span>";
+const Granos_Quinua = "</br>"+"<span>Granos de Quinua sin tostar</span>"+"</br>"+"<span>Granos de quinua pelados y lavados</span>";
+const Granos_Trigo= "</br>"+"<span>Granos de trigo sin tostar</span>"+"</br>"+"<span>Granos de trigo lavados y pelados</span>";
+const Granos_Sacha = "</br>"+"<span>Granos de Sacha Inchi sin pelas</span>"+"</br>"+"<span>Granos lavados y descontaminados</span>";
+const Granos_Mani = "</br>"+"<span>Harina de mani pelados</span>"+"</br>"+"<span>Granos lavados son tostar</span>";
+const Harina_Maiz = "</br>"+"<span>Harina de granos de maiz amarillos</span>"+"</br>"+"<span>Harina de maiz seco</span>";
+const Castañas = "</br>"+"<span>Granos de castaña lavados</span>"+"</br>"+"<span>Granos sin tostar</span>"
+
+
+
+
     document.querySelector("h3").addEventListener("click", ()=>{
         var Span = document.querySelector(".des").innerText;
-
         switch (Span) {
-            case 'Chalina echa con lana de llama Cusqueña':
-                document.querySelector(".features").innerHTML ="</br>"+ LLana_Ove+"</br>"+"<span>Posee varios colores</span>";
+            case 'Harina de habas':
+                document.querySelector(".features").innerHTML =Harina_Habas;
                 break;
-            case 'Lana de llama':
-                document.querySelector(".features").innerHTML = h3+"</br>"+ LLana_Ove+"</br>"+"<span>Posee varios colores</span>"
+            case 'Kañiwa tostada':
+                document.querySelector(".features").innerHTML = Kañiwa_tostada;
                 break;
-            
-            default:
+            case 'Granos de Quinua':
+                document.querySelector(".features").innerHTML = Granos_Quinua;
+                break;
+            case 'Granos de Trigo':
+                document.querySelector(".features").innerHTML=Granos_Trigo;
+                break;
+            case 'Granos de Sacha inchi':
+                document.querySelector(".features").innerHTML = Granos_Sacha;
+                break;
+            case 'Granos de mani pelados':
+                document.querySelector(".features").innerHTML=Granos_Mani;
+                break;
+            case 'Harina de maiz':
+                document.querySelector(".features").innerHTML=Harina_Maiz;
+                break;
+            case 'Castañas':
+                document.querySelector(".features").innerHTML=Castañas;
                 break;
         }
             
@@ -18,25 +45,9 @@ var Count = 0;
 
     document.querySelector(".fa-shopping-cart").addEventListener("click", ()=>{
         if(Count==1)return;
-        document.querySelector(".precio").innerHTML=document.querySelector(".precio").innerHTML+12;
-        document.querySelector(".precio").classList.remove("display");
+        document.querySelector(".precio").innerHTML=document.querySelector(".precio").innerHTML+"S/"+12;
+        document.querySelector(".cont").classList.remove("display");
         Count=1;
-
     })
-    var Llana_llma =  "<span>echa con lana de llama</span>";
-    var LLana_Ove =  "<span>echa con lana de oveja</span>";
-    var color = "<span>Posee un color </span>";
-
-
-    /*const Descripcion = {
-        'Chalina echa con lana de llama Cusqueña': document.querySelector(".features").innerHTML = h3+"</br>"+ Llana_llma+"</br>"+ "<span>Posee varios colores</span>",
-        'Lana de llama' : document.querySelector(".features").innerHTML = h3+"</br>"+ LLana_Ove+"</br>"+"<span>Posee varios colores</span>",
-        'Saco de lana para mujer': document.querySelector(".features").innerHTML=h3+"</br>"+Llana_llma+"</br>"+color+"marron",
-        'Set de crochetes para tejer': document.querySelector(".features").innerHTML = h3+"</br>"+"Posee un set de 5 crochetes, agujas y ganchos" + "</br>"+ "Posee varios colores",
-        'Guantes blancos de lana' : document.querySelector(".features").innerHTML=h3+"</br>"+LLana_Ove+"</br>"+color+"blanco",
-        'Gorro de lana': document.querySelector(".feutures").innerHTML=h3+"</br>"+LLana_Ove+"</br>"+color+"marron",
-
-
-    }*/
 
 
